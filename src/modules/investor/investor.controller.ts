41fa985f6d97fd0082bc2688c2930cd169c40e51
Asror <khberv.dev@gmail.com>
2025-12-13 18:29:40 +0500

    feat: add contact, vacancy

--- src/modules/investor/investor.controller.ts
+++ src/modules/investor/investor.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import JwtAuthGuard from '../../helpers/guards/jwt-auth.guard';
 import InvestorService from './investor.service';
 import { User } from '../../helpers/decorators/user.decorator';
+import CreateContactDto from '../legal/dto/create-contact.dto';
+import CreateVacancyDto from '../legal/dto/create-vacancy.dto';
 
 @Controller('investor')
 @UseGuards(JwtAuthGuard)
@@ -11,5 +13,25 @@ export default class InvestorController {
   @Get('me')
   async getMe(@User() user: User) {
     return this.investorService.getMe(user.roleUID);
+  }
+
+  @Post('contacts')
+  async addContact(@User() user: User, @Body() body: CreateContactDto) {
+    return this.investorService.createContact(user.roleUID, body);
+  }
+
+  @Get('vacancies')
+  async getVacancies(@User() user: User) {
+    return this.investorService.getVacancies(user.roleUID);
+  }
+
+  @Get('vacancies/:id')
+  async getVacancyById(@Param('id') id: string) {
+    return this.investorService.getVacancyById(id);
+  }
+
+  @Post('create-vacancy')
+  async createVacancy(@User() user: User, @Body() body: CreateVacancyDto) {
+    return this.investorService.createVacancy(user.roleUID, body);
   }
 }
--- src/modules/investor/investor.service.ts
+++ src/modules/investor/investor.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import InvestorRepository from './investor.repository';
+import CreateContactDto from '../legal/dto/create-contact.dto';
+import DatabaseService from '../database/database.service';
+import CreateVacancyDto from '../legal/dto/create-vacancy.dto';
+import UpdateVacancyDto from '../legal/dto/update-vacancy.dto';
 
 @Injectable()
 export default class InvestorService {
-  constructor(private investorRepository: InvestorRepository) {}
+  constructor(
+    private investorRepository: InvestorRepository,
+    private databaseService: DatabaseService,
+  ) {}
 
   async getMe(id: string) {
     const legal = await this.investorRepository.findById(id);
@@ -11,6 +18,95 @@ export default class InvestorService {
     return {
       ok: true,
       data: legal,
+    };
+  }
+
+  async createContact(investorId: string, data: CreateContactDto) {
+    await this.databaseService.contact.create({
+      data: {
+        investorId,
+        contact: data.contact,
+        person: data.person,
+        type: data.type,
+      },
+    });
+
+    return {
+      ok: true,
+      message: {
+        uz: "Kontakt qo'shildi",
+      },
+    };
+  }
+
+  async createVacancy(investorId: string, data: CreateVacancyDto) {
+    await this.databaseService.vacancy.create({
+      data: {
+        investorId,
+        title: data.title,
+        salary: data.salary,
+        description: data.description,
+      },
+    });
+
+    return {
+      ok: true,
+      message: {
+        uz: 'Vakansiya yaratildi',
+      },
+    };
+  }
+
+  async updateVacancy(vacancyId: string, data: UpdateVacancyDto) {
+    await this.databaseService.vacancy.update({
+      where: {
+        id: vacancyId,
+      },
+      data,
+    });
+
+    return {
+      ok: true,
+      message: {
+        uz: "Ma'lumot yangilandi",
+      },
+    };
+  }
+
+  async getVacancies(investorId: string) {
+    const vacancies = await this.databaseService.vacancy.findMany({
+      where: {
+        investorId,
+        deletedAt: null,
+      },
+    });
+
+    return {
+      ok: true,
+      data: vacancies,
+    };
+  }
+
+  async getVacancyById(vacancyId: string) {
+    const vacancy = await this.databaseService.vacancy.findFirst({
+      where: {
+        id: vacancyId,
+        deletedAt: null,
+      },
+    });
+
+    if (!vacancy) {
+      return {
+        ok: false,
+        message: {
+          uz: "Ma'lumot topilmadi",
+        },
+      };
+    }
+
+    return {
+      ok: true,
+      data: vacancy,
     };
   }
 }
